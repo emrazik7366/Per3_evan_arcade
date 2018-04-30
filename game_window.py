fd45d82f441593b2c9d3df70cc3ff9d9c0deb415
Evan Mrazik <EMrazik7366@student.pps.net>
2018-04-30 13:26:09 -0700

    display a window with a white background color

--- game_window.py
+++ game_window.py
@@ -11,6 +11,8 @@ class MyGame(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
+        arcade.set_background_color(arcade.color.WHITE)
+
 
         # If you have sprite lists, you should create them here,
         # and set them to None
@@ -24,7 +26,7 @@ class MyGame(arcade.Window):
         Render the screen.
         """
         arcade.start_render()
-
+		
   
 
 def main():
