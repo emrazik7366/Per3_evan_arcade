dab27de0d37373699c3ab88939c51276d7f057d5
Evan Mrazik <EMrazik7366@student.pps.net>
2018-04-30 13:28:30 -0700

    display a window with some other interesting background color

--- game_window.py
+++ game_window.py
@@ -11,7 +11,7 @@ class MyGame(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
-        arcade.set_background_color(arcade.color.WHITE)
+        arcade.set_background_color(arcade.color.KOBI)
 
 
         # If you have sprite lists, you should create them here,
