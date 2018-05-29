0dcf36ee7a5aff2d2fdc7607dc06a0f26df9ad04
Holden Hindes <HHindes8212@student.pps.net>
2018-05-29 13:26:03 -0700

    adding holden's breadboard functions

---
+++ game_button_and_led.py
@@ -0,0 +1,16 @@
+from gpiozero import Button
+from time import sleep
+from Per3_evan_arcade import arcade_final_code
+def buttonn():
+    sensor = Button(2)
+    while 1 == 1:
+        sensor.wait_for_press()
+        #jump
+        print('jumped')
+        sleep(.5)
+def ledd():
+    led = LED(17)
+    led.off()
+    while 1 == 1:
+        if self.game_over == True:
+            led.on()
